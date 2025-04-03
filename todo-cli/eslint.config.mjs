9ef15275dbdd753c4eb2c87484a6b88f55711b97
Anantha Chary C M <ananthacm2005@gmail.com>
2025-04-03 10:54:55 +0000

    changes

--- todo-cli/eslint.config.mjs
+++ todo-cli/eslint.config.mjs
@@ -1,11 +1,14 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import js from "@eslint/js";
-import jest from "eslint-plugin-jest";
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"] },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.test.js", "**/__tests__/**/*.js"], languageOptions: { globals: globals.jest } },
-  { files: ["**/*.js"], plugins: { js, jest }, extends: ["js/recommended", "plugin:jest/recommended"] },
-]);
+{
+  "env": {
+    "commonjs": true,
+      "es2021": true,
+        "node": true,
+          "jest": true
+  },
+  "extends": ["eslint:recommended", "plugin:jest/recommended"],
+    "overrides": [],
+      "parserOptions": {
+    "ecmaVersion": "latest"
+  },
+  "rules": { }
+}
