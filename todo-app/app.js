0ba441142bc29441cc098cf7bf7ecebc5fdf2920
Anantha Chary C M <ananthacm2005@gmail.com>
2025-04-07 19:22:43 +0000

    l7

--- todo-app/app.js
+++ todo-app/app.js
@@ -12,7 +12,7 @@ app.get("/", async (request, response) => {
     const allTodos = await Todo.findAll();
     if (request.accepts("html")) {
       response.render("index", {
-        allTodos
+        todos: allTodos
       });
     } else {
       response.json({ allTodos });
